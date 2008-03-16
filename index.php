1e13152fe304bfcf0ee2a1346924b13e91b40ef8
ddunne <ddunne>
2008-03-16 01:08:07 +0000

    *** empty log message ***

--- index.php
+++ index.php
@@ -32,11 +32,9 @@ include ($App->getProjectCommon()); # All on the same line to unclutter the user
 			verification, and validation.
 
       <a href="http://www.eclipse.org/downloads/download.php?file=/technology/osee/2.0%20OSEE%20Overview%20Screencast.zip">OSEE Overview</a> - This screencast provides an overview of OSEE including the driving forces that caused it's creation, it's architecture, the maturity of the application framework and the status and maturity of the different applications that use the framework.<br>
-	</div>
-	
-	<div align="center">
-      <a href="http://en.wikipedia.org/wiki/V_model"><img align="center" src="images/VDiagram_sm.png" border="0" />
-	   </a>
+
+      <a href="http://en.wikipedia.org/wiki/V_model"><img align="center" src="images/VDiagram_sm.png" border="0" /></a>
+      
 	</div>		
 	
 	<div id="rightcolumn">
