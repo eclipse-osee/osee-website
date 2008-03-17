cca7afe8d8318842442382ba3c6eff0f3ee1e2ed
ddunne <ddunne>
2008-03-17 15:57:08 +0000

    *** empty log message ***

---
+++ README.txt
@@ -0,0 +1,4 @@
+The files in this directory support the www.eclipse.org/osee website
+
+NOTE: The *.html files exist ONLY to aid in creation of the pages.  After editing in these, the changes MUST be copied
+into the corresponding *.php file before commit.
---
+++ downloads.php
@@ -0,0 +1,38 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	# faq.php
+	#****************************************************************************
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "OSEE Downloads";
+	$pageKeywords	= "OSEE, Open, System, Engineering, Environment, Eclipse, Define, ATS, OTE, Application Framework";
+	$pageAuthor		= "Ryan D. Brooks; Donald G. Dunne";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+	# End: page-specific settings
+	#
+		
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+
+	<div id="midcolumn">
+		<h1>$pageTitle</h1>
+		OSEE is currently available either through access to the OSEE source committed to the Eclipse project
+		or through the Demos provided on this site.<br><br>Please contact us for further information on how to use, extend
+		or collaborate on OSEE's development.
+
+	</div>
+	
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
