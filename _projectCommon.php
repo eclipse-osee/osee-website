e5740e780ac7ce28a56f38799f7a34d549c17c98
ddunne <ddunne>
2008-03-27 20:42:18 +0000

    *** empty log message ***

--- _projectCommon.php
+++ _projectCommon.php
@@ -17,6 +17,7 @@
 	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;News and Events", "/osee/news.php", 	"_self", 1);
 	$Nav->addNavSeparator("Development", 	"");
 	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;Newsgroup", "http://www.eclipse.org/newsportal/thread.php?group=eclipse.technology.osee", "_blank", 1);
+	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;Bugs", "https://bugs.eclipse.org/bugs/buglist.cgi?product=OSEE", "_blank", 1);
 	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;Architecture", "/osee/arch.php", "_self", 1);
 	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;Workspace Setup", "/osee/devSetup.php", "_self", 1);
 	$Nav->addNavSeparator("Other", 	"");
