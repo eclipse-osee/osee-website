71a320b5b0a784f6ab191309232758f36291fe72
ddunne <ddunne>
2008-03-27 01:46:59 +0000

    *** empty log message ***

--- devSetup.php
+++ devSetup.php
@@ -14,16 +14,16 @@
       here is not accurate or missing.
       
       <ol>
-      <li>Install Java JDK 1.6
-      <li>Install Eclipse 3.3
-      <li>Startup Eclipse, select workspace
+      <li>Install Java JDK 1.6<br>
+      <li>Install Eclipse 3.3<br>
+      <li>Startup Eclipse, select workspace<br>
       <li>Add SVN client to Eclipse by following instructions at <a href="http://www.polarion.org/index.php?page=download&project=subversive">
       http://www.polarion.org/index.php?page=download&project=subversive</a>. You will
       want to add all plugins available at Note:  This will allow you to connect to the OSEE SVN repositories AND OSEE uses some of these 
-      plugins for compiling.
+      plugins for compiling.<br>
       <li>Connect to the OSEE SVN repository and checkout all plugins.  You will connect using your committer username password or do an
       anonymous connection if you are not an OSEE committer.  Instructions are here <a href="http://wiki.eclipse.org/SVN_Howto">http://wiki.eclipse.org/SVN_Howto</a>.
-      <li>OSEE uses the GEF's Zest Visualization plugins.  To resolve these dependencies, you will need to anonymously connect to the /cvsroot/tools 
+      <br><li>OSEE uses the GEF's Zest Visualization plugins.  To resolve these dependencies, you will need to anonymously connect to the /cvsroot/tools 
       CVS repository, expand out the org.eclipse.gef project and check out org.eclilpse.zest.core and org.eclipse.zest.layouts projects.  
       Instructions on doing this are at <a href="http://wiki.eclipse.org/index.php/CVS_Howto">http://wiki.eclipse.org/index.php/CVS_Howto</a> 
       </ol>
