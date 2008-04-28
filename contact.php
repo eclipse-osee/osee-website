343c4a3442c2dd6cae6b2755a3d57e9713a4b6cd
ddunne <ddunne>
2008-04-28 16:11:53 +0000

    *** empty log message ***

--- contact.php
+++ contact.php
@@ -36,7 +36,7 @@
 <p><table border="0" width="100%"><tr >
 <td width="33%" height="200" align="center" valign="bottom"><img border="0" src="images/team/eclipseface.png"/><br/>Paul Walfogel<br/>Boeing<br/>Define</td>
 <td width="33%" height="200" align="center" valign="bottom"><img border="0" src="images/team/AndyJury.jpg"/><br/>Andy Jury<br/>Boeing<br/>Test</td>
-<td width="33%" height="200" align="center" valign="bottom"><img border="0" src="images/team/eclipseface.png"/><br/>Theron Virgin<br/>Boeing<br/>Define, Test</td>
+<td width="33%" height="200" align="center" valign="bottom"><img border="0" src="images/team/TheronVirgin.jpg"/><br/>Theron Virgin<br/>Boeing<br/>Define, Test</td>
 </tr><tr>
 <td width="33%" height="200" align="center" valign="bottom"><img border="0" src="images/team/eclipseface.png"/><br/>Joel Rosi-Schwartz<br/>Etish Limited<br/>Define, ORMF</td>
 <td width="33%" height="200" align="center" valign="bottom"><img border="0" src="images/team/eclipseface.png"/><br/>Barbara Rosi-Schwartz<br/>Etish Limited<br/>Define, ORMF</td>
