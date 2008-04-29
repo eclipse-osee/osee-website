ed89141c57a47cd4c1c309d80f05f12bff27eaca
ddunne <ddunne>
2008-04-29 14:00:10 +0000

    *** empty log message ***

--- _projectCommon.php
+++ _projectCommon.php
@@ -12,6 +12,7 @@
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	$Nav->addCustomNav("About This Project", "http://www.eclipse.org/projects/project_summary.php?projectid=technology.osee", "_self", 1);
 	$Nav->addNavSeparator("Getting Started", 	"/osee/index.php");
+	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;Components", "/osee/components.php", "_self", 1);
 	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;Demos", "/osee/demos.php", "_self", 1);
 	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;Downloads", "/osee/downloads.php", "_self", 1);
 	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;&nbsp;FAQ", "/osee/faq.php", 	"_self", 1);
