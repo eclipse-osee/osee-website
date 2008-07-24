a39db8011fb8fb2f26ce9f58f9b5223aaee5a7e9
rescobar <rescobar>
2008-07-24 17:36:24 +0000

    *** empty log message ***

---
+++ documentation/gettingStarted/aboutOsee/install.php
@@ -0,0 +1,41 @@
+<?php
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");
+	$App = new App();
+	$Nav = new Nav();
+	$Menu = new Menu();
+	include($App->getProjectCommon());
+	# All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	# requirements.php
+	#
+	#****************************************************************************
+
+	#
+	# Begin: page-specific settings.  Change these.
+	$pageTitle 		= "OSEE - Documentation";
+	$pageKeywords	= "OSEE, Open, System, Engineering, Environment, Eclipse, Define, ATS, OTE, Application Framework";
+	$pageAuthor		= "Roberto E. Escobar";
+
+	# Paste your HTML content between the EOHTML markers!
+	$html = <<<EOHTML
+
+	<div id="midcolumn" style="width:95%">
+
+		<h3>Install</h3>
+		<p>
+			Instructions here - TODO<br/>
+		</p>
+	<br/>
+	<br/>
+	</div>
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
---
+++ documentation/gettingStarted/aboutOsee/requirements.php
@@ -0,0 +1,44 @@
+<?php
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");
+	$App = new App();
+	$Nav = new Nav();
+	$Menu = new Menu();
+	include($App->getProjectCommon());
+	# All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	# requirements.php
+	#
+	#****************************************************************************
+
+	#
+	# Begin: page-specific settings.  Change these.
+	$pageTitle 		= "OSEE - Documentation";
+	$pageKeywords	= "OSEE, Open, System, Engineering, Environment, Eclipse, Define, ATS, OTE, Application Framework";
+	$pageAuthor		= "Roberto E. Escobar";
+
+	# Paste your HTML content between the EOHTML markers!
+	$html = <<<EOHTML
+
+	<div id="midcolumn" style="width:95%">
+
+		<h3>Requirements</h3>
+		<p>
+			JDK 1.6 or higher<br/>
+			Eclipse 3.4<br/>
+			GEF 3.4<br/>
+			Equinox 3.4 Add-on Bundles<br/>
+		</p>
+	<br/>
+	<br/>
+	</div>
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
