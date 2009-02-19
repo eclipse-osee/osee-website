5bdfc1cbbbe9fb66fc75c4fed3b74a80e16467b1
rescobar <rescobar>
2009-02-19 00:21:33 +0000

    *** empty log message ***

--- documentation/configuration/dbconnection.php
+++ documentation/configuration/dbconnection.php
@@ -34,26 +34,26 @@
 	&lt;?xml version=&quot;1.0&quot; encoding=&quot;UTF-8&quot;?&gt;
 	&lt;DbConnection&gt;
 	
-	&lt;DatabaseInfo id=&quot;postgresqlInfo&quot;&gt;
+	&lt;DatabaseInfo id=&quot;<font color="green">postgresqlInfo</font>&quot;&gt;
 		&lt;DatabaseHome key=&quot;#DBHOME#&quot; /&gt;
-		&lt;DatabaseName key=&quot;#DBNAME#&quot; value="OSEE" /&gt;
+		&lt;DatabaseName key=&quot;#DBNAME#&quot; value=&quot;<b>DATABASE NAME</b>&quot; /&gt;
 		&lt;DatabaseType key=&quot;#TYPE#&quot; value="postgresql" /&gt;
 		&lt;Prefix key=&quot;#PREFIX#&quot; value="jdbc:postgresql" /&gt;
-		&lt;UserName key=&quot;#USERNAME#&quot; value="osee" /&gt;
-		&lt;Password key=&quot;#PASSWORD#&quot; value="osee" /&gt;
+		&lt;UserName key=&quot;#USERNAME#&quot; value=&quot;<b>DATABASE USERNAME</b>&quot; /&gt;
+		&lt;Password key=&quot;#PASSWORD#&quot; value=&quot;<b>DATABASE PASSWORD</b>&quot; /&gt;
 		&lt;Host key=&quot;#HOST#&quot; value="@AvailableDbServices.hostAddress" /&gt;
 		&lt;Port key=&quot;#PORT#&quot; value="@AvailableDbServices.port" /&gt;
 	&lt;/DatabaseInfo&gt;
 
-	&lt;ConnectionDescription id=&quot;postgresql&quot; &gt;
+	&lt;ConnectionDescription id=&quot;<font color="blue">postgresql</font>&quot; &gt;
 		&lt;Driver>org.postgresql.Driver&lt;/Driver&gt;
 		&lt;Url>#PREFIX#://#HOST#:#PORT#/#DBHOME##DBNAME#&lt;/Url&gt;		
 	&lt;/ConnectionDescription&gt;
 
 	&lt;AvailableDbServices>
-		&lt;Server id=&quot;<b>ANY_NAME</b>&quot; dbInfo=&quot;postgresqlInfo&quot;
-			hostAddress=&quot;<b>DATABASE_IP_ADDRESS</b>&quot; port=&quot;<b>DATABASE_PORT</b>&quot;
-			connectsWith=&quot;postgresql&quot; isProduction=&quot;false&quot; /&gt;
+		&lt;Server id=&quot;<b>ANY NAME</b>&quot; dbInfo=&quot;<font color="green">postgresqlInfo</font>&quot;
+			hostAddress=&quot;<b>DATABASE IP ADDRESS</b>&quot; port=&quot;<b>DATABASE PORT</b>&quot;
+			connectsWith=&quot;<font color="blue">postgresql</font>&quot; isProduction=&quot;false&quot; /&gt;
 	&lt;/AvailableDbServices&gt;
 	&lt;/DbConnection&gt;
 	</pre>
