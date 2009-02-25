189485935cf070f1ac2ffc2269f9a4815a1b5300
rbrooks <rbrooks>
2009-02-25 17:36:38 +0000

    *** empty log message ***

---
+++ documentation/test.php
@@ -0,0 +1,32 @@
+<?php
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");
+	$App = new App();
+	$Nav = new Nav();
+	$Menu = new Menu();
+	include($App->getProjectCommon());
+	
+	# All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	#****************************************************************************
+
+	#
+	# Begin: page-specific settings.  Change these.
+	$pageTitle 		= "OSEE - Data Model";
+	$pageKeywords	= "OSEE, Open, System, Engineering, Environment, Eclipse, Define, ATS, OTE, Application Framework, Install";
+	$pageAuthor		= "Roberto E. Escobar";
+
+	# Paste your HTML content between the EOHTML markers!
+	$html = <<<EOHTML
+
+	<div id="midcolumn" style="width:95%">
+		<h3>$pageTitle</h3>
+	</div>
+EOHTML;
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
