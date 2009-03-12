790de4f6fee8d014b362e2004f28cdd1c8af8e27
rbrooks <rbrooks>
2009-03-12 18:56:27 +0000

    *** empty log message ***

--- downloads.php
+++ downloads.php
@@ -69,9 +69,11 @@
                 	<ul>
 							Required dependencies are included in the Ganymede Eclipse install <a href="http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/ganymede/SR2/eclipse-reporting-ganymede-SR2-win32.zip"> Eclipse IDE for Java and Report Developers</a>.<br/>
 							Just for reference, these dependencies include: org.eclipse.gef, org.eclipse.draw2d, org.eclipse.birt, org.eclipse.datatools, and org.apache.derby.core.
-                 </ul>
+                 </ul><br/>
                  <i>Important: If you would like to work with a database other than Derby, you will also need to install JDBC Drivers distributed from external location. Such scheme of distribution is caused by licensing requirements.</i><br/>
-                 <img src="$arrowImage"></img>&nbsp;&nbsp;<b><a href="$externalDownloadsSite">Visit external site to get add-ons</a> and look for the link labeled Add-Ons Archived Update Site</b>
+                 <img src="$arrowImage"></img>&nbsp;&nbsp;<b><a href="$externalDownloadsSite">Visit external site to get add-ons</a></b> and look for the link labeled Add-Ons Archived Update Site.<br/>
+                 When launching the client you must set the following java system properties (i.e. include them in the command used to launch eclipse) -Dosee.authentication.protocol=trustAll -Dosee.arbitration.server=http://localhost:8089.  
+                 <br/>
                 </li>
                 <li>
                 	<b>All-In-One</b><br/>
