0126b7db5bac835382584c7dde1362ad86f10ee8
rbrooks <rbrooks>
2009-03-17 00:12:45 +0000

    *** empty log message ***

--- documentation/installation/requirements.php
+++ documentation/installation/requirements.php
@@ -26,15 +26,26 @@
 	<div id="midcolumn">
 
 		<h3>$pageTitle</h3>
-		<p>
-			JDK 1.6 or higher<br/>
-			Eclipse 3.4<br/>
-			GEF 3.4<br/>
-			Equinox 3.4 Add-on Bundles<br/>
-			Relational Database such as PostgreSQL, MySQL, Oracle, or Derby <br/>
-		</p>
-	<br/>
-	<br/>
+		
+		<h4>System Requirements (non-Eclipse)</h4>
+		<ul>
+			<li>JDK 1.6 or higher</li>
+			<li>Relational Database such as PostgreSQL, MySQL, Oracle, or Derby</li>
+		</ul>
+		<h4>Eclipse Requirements</h4>
+		<ul>
+			<li>An Eclipse 3.4.1 base with the following features installed</li>
+			<li>
+				<ul>
+					<li>org.eclipse.gef</li>
+					<li>org.eclipse.draw2d</li>
+					<li>org.eclipse.birt</li>
+					<li>org.eclipse.datatools</li>
+					<li>org.apache.derby.core</li>
+				</ul>
+			</li>
+			<li>The easiest solution is the Ganymede Eclipse install <a href="http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/ganymede/SR2/eclipse-reporting-ganymede-SR2-win32.zip"> Eclipse IDE for Java and Report Developers</a>.</li>
+	</ul>
 	</div>
 
 EOHTML;
