5cd095f1b700bb28132331a5e865b46fea1fb929
rbrooks <rbrooks>
2009-03-17 03:06:11 +0000

    *** empty log message ***

--- documentation/installation/requirements.php
+++ documentation/installation/requirements.php
@@ -21,8 +21,9 @@
 		
 		<h4>System Requirements (non-Eclipse)</h4>
 		<ul>
-			<li>JDK 1.6 or higher</li>
-			<li>Relational Database such as PostgreSQL, MySQL, Oracle, or Derby</li>
+			<li>JRE 1.6 or higher</li>
+			<li>Relational Database such as PostgreSQL, MySQL, Oracle, or Derby.<br/>
+			See instructions for installing <a href="supported_databases.php">'Supported Databases'</a> to complete this step.</li>
 		</ul>
 		<h4>Eclipse Requirements</h4>
 		<ul>
@@ -34,14 +35,14 @@
 				<li>org.eclipse.datatools</li>
 				<li>org.apache.derby.core</li>
 			</ul>
-			The easiest solution is the Ganymede Eclipse install <a href="http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/ganymede/SR2/eclipse-reporting-ganymede-SR2-win32.zip"> Eclipse IDE for Java and Report Developers</a> since it provides all these Eclipse dependencies
+			The easiest solution is the Ganymede Eclipse install <a href="http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/ganymede/SR2/eclipse-reporting-ganymede-SR2-win32.zip"> Eclipse IDE for Java and Report Developers</a> since it provides the above Eclipse dependencies.<br/> 
+			Unzip the content of this zip file into a (short) path on you computer to create your Eclipse base.
 			<li>OSEE add-ons - <a href="$externalDownloadsSite">Visit external site under the section &quot;Add-On Features&quot;</a><br/>
 			To use OSEE with a database other than Derby, you will need to install JDBC Drivers distributed from this external location. Such scheme of distribution is caused by licensing requirements.</li>
 		</ul>
 	</div>
 
 EOHTML;
-
 
 	# Generate the web page
 	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
