7ecc64d83a97c5f764341101700b33252c1b3545
rbrooks <rbrooks>
2009-03-23 15:36:45 +0000

    *** empty log message ***

--- documentation/installation/requirements.php
+++ documentation/installation/requirements.php
@@ -28,16 +28,16 @@
 		</ul>
 		<h4>Eclipse Dependencies</h4>
 		<ol>
-			<li>An Eclipse 3.4.1 base with the following features installed</li>
+			<li>An Eclipse 3.4.1 base with the following features installed<br/>
+			<i>you can which featuers are already installed from Help -> About Eclipse Platform ->Feature Details</i>
 			<ul>
-				<li>org.eclipse.gef</li>
-				<li>org.eclipse.draw2d</li>
-				<li>org.eclipse.birt</li>
-				<li>org.eclipse.datatools</li>
-				<li>org.apache.derby.core</li>
+				<li><a href="http://www.eclipse.org/gef/downloads/">org.eclipse.gef and org.eclipse.draw2d</a></li>
+				<li><a href="http://www.eclipse.org/datatools/downloads.php">org.eclipse.datatools.connectivit.oda.feature and org.eclipse.datatools.enablement.oda.feature</a></li>
+				<li><a href="http://download.eclipse.org/birt/downloads/">org.eclipse.birt.chart.runtime</a></li>	
 			</ul>
 			The easiest solution is the Ganymede Eclipse install <a href="http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/ganymede/SR2/eclipse-reporting-ganymede-SR2-win32.zip"> Eclipse IDE for Java and Report Developers</a> since it provides the above Eclipse dependencies.<br/> 
 			Unzip the contents of this zip file into a (short) path on you computer to create your Eclipse base.
+			</li>
 			<li>OSEE add-ons - <a href="$externalDownloadsSite">Visit external site under the section &quot;Add-On Features&quot;</a><br/>
 				From the Eclipse base installed in the previous step, launch the update manager and install the add-ins archived update site (no need to unzip it).  For more detail, see step-by-step <a href="$installInstructions">'Update Manager Instructions'</a>.<br/>
 				<br/>
