a5130c69b270c22b2887287b53efe77f3fc03a06
rschmitt <rschmitt>
2010-03-31 22:40:07 +0000

    *** empty log message ***

--- _projectCommon.php
+++ _projectCommon.php
@@ -15,18 +15,18 @@
 
 	$Nav->addNavSeparator("OSEE Home", "/osee/index.php");
 	$Nav->addCustomNav("Project Summary", "http://www.eclipse.org/projects/project_summary.php?projectid=technology.osee", "_self", 2);
-
-	$Nav->addNavSeparator("Documentation", "_blank");
-	$Nav->addCustomNav("Wiki", "http://wiki.eclipse.org/OSEE", "_self", 2);
-	$Nav->addCustomNav("Screencasts (wiki)", "http://wiki.eclipse.org/OSEE/Users_Guide#Screencasts", "_self", 2);
-	$Nav->addCustomNav("FAQ (wiki)", "http://wiki.eclipse.org/OSEE/Users_Guide#Frequently_Asked_Questions", "_self", 2);
-	
-	$Nav->addNavSeparator("Development", "_blank");
 	$Nav->addCustomNav("Downloads", "/osee/downloads.php", "_self", 2);
-	$Nav->addCustomNav("Repository", "https://dev.eclipse.org/svnroot/technology/org.eclipse.osee", "_self", 2);
+	
+	$Nav->addNavSeparator("Documentation", "");
+	$Nav->addCustomNav("Wiki", "http://wiki.eclipse.org/OSEE", "_self", 2);
+	$Nav->addCustomNav("Screencasts", "http://wiki.eclipse.org/OSEE/Users_Guide#Screencasts", "_self", 2);
+	$Nav->addCustomNav("FAQ", "http://wiki.eclipse.org/OSEE/Users_Guide#Frequently_Asked_Questions", "_self", 2);
+	
+	$Nav->addNavSeparator("Development", "");
+	$Nav->addCustomNav("Source Code", "https://dev.eclipse.org/svnroot/technology/org.eclipse.osee", "_self", 2);
 	$Nav->addCustomNav("Bugzilla", "https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&classification=Technology&product=OSEE&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED", "_self", 2);
 
-	$Nav->addNavSeparator("Discussion", "_blank");
+	$Nav->addNavSeparator("Discussion", "");
 	$Nav->addCustomNav("Mailing List", "https://dev.eclipse.org/mailman/listinfo/osee-dev", "_self", 2);
 	$Nav->addCustomNav("Forum", "http://www.eclipse.org/forums/index.php?t=thread&frm_id=68&S=03be8852279043e2fbe465ca71cbb7ac", "_self", 2);
 	$Nav->addCustomNav("Contacts", "/osee/contact.php", "_self", 2);
