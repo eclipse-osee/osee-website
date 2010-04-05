946f94e776b2f5cc9cdd289afd6b3e6269d739b7
rschmitt <rschmitt>
2010-04-05 18:38:32 +0000

    *** empty log message ***

--- nova/index.php
+++ nova/index.php
@@ -16,9 +16,9 @@
 	$localVersion = false;
 	
 	# Define these here, or in _projectCommon.php for site-wide values
-	$pageKeywords	= "eclipse, project";
-	$pageAuthor		= "Your name";
-	$pageTitle 		= "My project home";
+	$pageKeywords	= "eclipse, osee";
+	$pageAuthor		= "Ryan Schmitt";
+	$pageTitle 		= "Open System Engineering Environment";
 	
 	
 	// 	# Paste your HTML content between the EOHTML markers!
