44d2400c37d16125a0596b828db212adc788c1a5
rescobar <rescobar>
2011-01-29 00:09:15 +0000

    *** empty log message ***

--- downloads/index.php
+++ downloads/index.php
@@ -13,9 +13,11 @@
 	$pageKeywords	= "OSEE, Open, System, Engineering, Environment, Eclipse, Define, ATS, OTE, Application Framework";
 	$pageAuthor		= "Roberto E. Escobar";
 	
+	$release_prefix = "Milestone Build";
+	$release_date = "19 May 2010";
 	$release_version      = "0.9.4";
-   $externalDownloadsSite = "http://osee.microdoc.com/node/2";
-   $configandargs="documentation/configuration/configandargs.php";
+   $external_downloads_site = "http://osee.microdoc.com/node/2";
+   $config_args="http://wiki.eclipse.org/OSEE/Developers_Guide#Configuration_Properties";
    
    #*****************************************************************************
 	
@@ -23,9 +25,11 @@
 	$html .= file_get_contents('_index.html');
 	$html .= "</div>";
 	
+	$html = preg_replace('/@RELEASE_PREFIX@/', $release_prefix, $html);
+	$html = preg_replace('/@RELEASE_DATE@/', $release_date, $html);
 	$html = preg_replace('/@RELEASE_VERSION@/', $release_version, $html);
-	$html = preg_replace('/@EXTERNAL_DOWNLOADS_SITE@/', $externalDownloadsSite, $html);
-	$html = preg_replace('/@CONFIG_AND_ARGS@/', $configandargs, $html);
+	$html = preg_replace('/@EXTERNAL_DOWNLOADS_SITE@/', $external_downloads_site, $html);
+	$html = preg_replace('/@CONFIG_AND_ARGS@/', $config_args, $html);
 	
 	# Generate the web page
 	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
